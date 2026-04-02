adf1429c6d09893ccf3e75376aa5903a6af0f6ea
Shankalpa-D <DuwadiShankalpa@gmail.com>
2026-04-02 11:52:51 -0400

    added text field and controller

--- lib/widgits/expenses.dart
+++ lib/widgits/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker1/models/expense.dart';
 import 'package:expense_tracker1/widgits/expenses_list/expenses_list.dart';
+import 'package:expense_tracker1/widgits/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,10 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+
   final List<Expense> _registeredExpenses = [
     Expense(
       title: "Gino's Pizza",
@@ -37,7 +42,12 @@ class _ExpensesState extends State<Expenses> {
       appBar: AppBar(
         title: const Text('Expense Tracker'),
 
-        actions: [IconButton(icon: const Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _openAddExpenseOverlay,
+          ),
+        ],
       ),
       body: Column(
         children: [

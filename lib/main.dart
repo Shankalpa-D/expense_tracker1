66fa93e2433984671a8dd04e47b1344d6d986aec
Shankalpa-D <DuwadiShankalpa@gmail.com>
2026-04-14 12:12:58 -0400

    Started theaming

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
-
 import 'package:expense_tracker1/widgits/expenses.dart';
 import 'package:flutter/material.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 187, 104, 202),
+);
+
 void main() {
-  runApp(const MaterialApp(home: const Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 18,
+          ),
+        ),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }

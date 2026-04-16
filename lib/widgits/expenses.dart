d075c0b9bdda3ed561a680f858f1fe5b8cf50ca1
Shankalpa-D <DuwadiShankalpa@gmail.com>
2026-04-16 11:51:30 -0400

    added the chart

--- lib/widgits/expenses.dart
+++ lib/widgits/expenses.dart
@@ -2,6 +2,8 @@ import 'package:expense_tracker1/models/expense.dart';
 import 'package:expense_tracker1/widgits/expenses_list/expenses_list.dart';
 import 'package:expense_tracker1/widgits/new_expense.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker1/widgits/chart/chart_bar.dart';
+import 'package:expense_tracker1/widgits/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -92,8 +94,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('Chart Goes here'),
-          const SizedBox(height: 30),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
